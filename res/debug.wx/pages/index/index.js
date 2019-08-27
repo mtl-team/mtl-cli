4c047dfca2425ead36fac3f93b9e1b54b89d0648
徐刚 <xugangm@yonyou.com>
2019-08-27 10:58:12 +0800

    update debug wx

--- res/debug.wx/pages/index/index.js
+++ res/debug.wx/pages/index/index.js
@@ -1,40 +1,18 @@
 Page({
   onLoad(option) {
-    let _this = this;
-    let baseUrl = 'http://mobile.yyuap.com:3000';
-    let url = `${baseUrl}/project.json`;
-    console.log("onLoad");
-    wx.login({
+    wx.request({
+      url: 'http://localhost:3000/project.json',
       success: res => {
-        let authCode = res.code;
-        if (authCode) {
-          wx.request({
-            url: url,
-            success: res => {
-              console.log('success', res);
-              if (res.statusCode == 200) {
-                let startPage = res.data.config.startPage;
-                let appCode = res.data.config.wxAppCode;
-                if (startPage && startPage.indexOf('http') == -1) {
-                  res.pageUrl = `${baseUrl}/${startPage}`;
-                } else {
-                  res.pageUrl = `${startPage}`;
-                }
-                res.pageUrl = `${res.pageUrl}?authCode=${authCode}&appCode=${appCode}`
-                _this.setData({
-                  url: res.pageUrl,
-                })
-                console.log('url=', _this.data.url);
-              }
-            },
-            fail: res => {
-              console.log('fail', res);
-            }
-          })
+        let { startPage, technologyStack } = res.data.config;
+        let port = 3000;
+        if (technologyStack === 'mdf') {
+          port = 3003;
         }
+        let url = `http://localhost:${port}/${startPage}`
+        this.setData({
+          url: url
+        })
       }
-    });
-   
-
+    })
   }
 })
