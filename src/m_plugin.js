79137c5907c3d2cd51b41b13ee24b46e6ff509cc
xugangm <xugangm@yonyou.com>
2020-04-07 14:15:56 +0800

    add upesn plugin

--- src/m_plugin.js
+++ src/m_plugin.js
@@ -1,8 +1,9 @@
 const inquirer = require("inquirer");
 const allPluginFile = require("../res/plugin.json");
+const allUPesnPluginFile = require("../res/upesnPlugin.json");
 const utils = require("./m_util.js");
 const mtldev = require("mtl-dev-sdk");
-
+const fse = require('fs-extra');
 const promptList = [
   {
     type: "list",
@@ -18,6 +19,7 @@ function addPlugin(pluginName) {
   if (!utils.isMtlProject()) {
     return;
   }
+
   if (pluginName) {
     mtldev.serchPlugins({ cordovaName: pluginName }, res => {
       if (res.code == 200) {
@@ -29,8 +31,42 @@ function addPlugin(pluginName) {
     });
     return;
   }
-  selectPlugin(allPluginFile);
+
+  var proj = JSON.parse(fse.readFileSync("./project.json").toString());
+  if(proj.config.sourceType=="default"){
+    promptList[0].message ="请选择插件模板:upesnPlugin是代表友空间壳插件；mtlPlugin是代表mtl默认插件。",
+    promptList[0].name = "pluginMode",
+    promptList[0].choices = ["upesnPlugin", "mtlPlugin"];
+    inquirer.prompt(promptList).then(answers => {
+      let sourceType = answers.pluginMode;
+      utils.consoleLog("pluginMode=="+sourceType);
+     // 更新project.json 文件的构建来源
+     proj.config.sourceType= sourceType;
+     fse.writeFileSync("./project.json", formatJson(proj),{flag:'w',encoding:'utf-8',mode:'0666'});
+     selectPluginJsonFile(sourceType);
+    });
+  }else{
+    var tempProj = JSON.parse(fse.readFileSync("./project.json").toString());
+    
+    selectPluginJsonFile(tempProj.config.sourceType);
+  }
+ 
 }
+
+function selectPluginJsonFile(sourceType) {
+  utils.consoleLog("sourceType=="+sourceType);
+  if(sourceType !="upesnPlugin"){
+    selectPlugin(allPluginFile);
+  }else{
+    selectPlugin(allUPesnPluginFile);
+  }
+}
+
+
+function formatJson(data) {
+  return JSON.stringify(data,null,4);
+}
+
 function selectPlugin(plugins) {
   utils.consoleLog(`plugins size : ${plugins.length}`);
   if (!plugins || plugins.length <= 0) return utils.consoleLog("没有可用模板");
@@ -53,6 +89,8 @@ function selectPlugin(plugins) {
   });
   console.log("_newPlugins: ", _newPlugins);
   promptList[0].choices = _newPlugins;
+  promptList[0].message ="请选择你要添加的插件:",
+  promptList[0].name = "cordovaName",
   inquirer.prompt(promptList).then(answers => {
     let name = answers.cordovaName;
     utils.consoleLog(name);
