12e286aaf2fafd57e3583005d08de5d2942ddcac
xugangm <xugangm@yonyou.com>
2020-05-07 11:33:38 +0800

    update upesn plugin

--- src/m_plugin.js
+++ src/m_plugin.js
@@ -20,36 +20,37 @@ function addPlugin(pluginName) {
     return;
   }
 
-  if (pluginName) {
+  // if (pluginName) {
     mtldev.serchPlugins({ pluginId: pluginName }, res => {
       if (res.code == 200) {
-        let plugins = res.data.plugins || [];
-        selectPlugin(plugins);
+        let plugins = res.data.plugins || [];    
+        // utils.consoleLog(`cli : ${JSON.stringify(plugins)}`);    
+         selectPlugin(plugins);
       } else {
         utils.consoleLog(`cli : ${JSON.stringify(res)}`);
       }
     });
     return;
-  }
+  // }
 
-  var proj = JSON.parse(fse.readFileSync("./project.json").toString());
-  if(proj.config.sourceType=="default"){
-    promptList[0].message ="请选择插件模板:upesnPlugin是代表友空间壳插件；mtlPlugin是代表mtl默认插件。",
-    promptList[0].name = "pluginMode",
-    promptList[0].choices = ["upesnPlugin", "mtlPlugin"];
-    inquirer.prompt(promptList).then(answers => {
-      let sourceType = answers.pluginMode;
-      utils.consoleLog("pluginMode=="+sourceType);
-     // 更新project.json 文件的构建来源
-     proj.config.sourceType= sourceType;
-     fse.writeFileSync("./project.json", formatJson(proj),{flag:'w',encoding:'utf-8',mode:'0666'});
-     selectPluginJsonFile(sourceType);
-    });
-  }else{
-    var tempProj = JSON.parse(fse.readFileSync("./project.json").toString());
+  // var proj = JSON.parse(fse.readFileSync("./project.json").toString());
+  // if(proj.config.sourceType=="default"){
+  //   promptList[0].message ="请选择插件模板:upesnPlugin是代表友空间壳插件；mtlPlugin是代表mtl默认插件。",
+  //   promptList[0].name = "pluginMode",
+  //   promptList[0].choices = ["upesnPlugin", "mtlPlugin"];
+  //   inquirer.prompt(promptList).then(answers => {
+  //     let sourceType = answers.pluginMode;
+  //     utils.consoleLog("pluginMode=="+sourceType);
+  //    // 更新project.json 文件的构建来源
+  //    proj.config.sourceType= sourceType;
+  //    fse.writeFileSync("./project.json", formatJson(proj),{flag:'w',encoding:'utf-8',mode:'0666'});
+  //    selectPluginJsonFile(sourceType);
+  //   });
+  // }else{
+  //   var tempProj = JSON.parse(fse.readFileSync("./project.json").toString());
     
-    selectPluginJsonFile(tempProj.config.sourceType);
-  }
+  //   selectPluginJsonFile(tempProj.config.sourceType);
+  // }
  
 }
 
@@ -71,28 +72,29 @@ function selectPlugin(plugins) {
   utils.consoleLog(`plugins size : ${plugins.length}`);
   if (!plugins || plugins.length <= 0) return utils.consoleLog("没有可用模板");
   let _newPlugins = plugins.map(item => {
-    let { pluginId, pluginId: id, userId: owner } = item;
+    let { pluginId, pluginId: id, userId: owner ,version: version} = item;
     const newItem = {
       name: pluginId,
       value: {
         id,
+        version ,
         name: item.name || pluginId,
         owner,
-        parameters: item.parameters || [],
+        parameters: JSON.parse(item.parameters.toString()) || [],
         description: item.description || ""
       }
     };
 
-    console.log("new: ", newItem);
+    // console.log("new: ", newItem);
     return newItem;
   });
-  console.log("_newPlugins: ", _newPlugins);
+  // console.log("_newPlugins: ", _newPlugins);
   promptList[0].choices = _newPlugins;
   promptList[0].message ="请选择你要添加的插件:",
   promptList[0].name = "pluginId",
   inquirer.prompt(promptList).then(answers => {
     let name = answers.pluginId;
-    utils.consoleLog(name);
+    // utils.consoleLog(name);
     let ret = mtldev.setMTLPlugin(name);
     utils.consoleLog(ret);
     utils.consoleLog("操作完成");
